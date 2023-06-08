bceb9f3d388d55557ae26b4c3d386db9eb428ef0
ballicipluck <shyam4564@gmail.com>
2023-06-08 20:46:07 +0530

    test dis2

--- server.js
+++ server.js
@@ -2,5 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use('/', express.static('dist'));
-app.listen(process.env.PORT || 3000, function () {console.log('Example app listening on port 3000!');});
+// If users visit url/v2, they should see the dis2 content
+app.use('/v2', express.static('dist2'));
+app.listen(8878, function () {console.log('Example app listening on port ' + 8878);});
 
